9ce08e44a5e5e1193803c703b84c545f885c7b19
Prima Aulia Gusta <primAulia@gmail.com>
2020-02-11 18:56:57 +0100

    latest with individual url

--- scraper.rb
+++ scraper.rb
@@ -3,37 +3,47 @@ require 'nokogiri'
 
 def fetch_movies_url
   # scrape https://www.imdb.com/chart/top
-
+  base_url = 'http://www.imdb.com'
   top_url = []
 
-  # identify which class to scrape
-  # target the class using nokogiri method... #search
-  # #search output is an array
-  # take only first 5 with #first method #take
-  # iterate the attribute --> href
-  # push the value of href into top_url
-  
+  html_file = open('https://www.imdb.com/chart/top').read
+  html_doc = Nokogiri::HTML(html_file)
+
+  html_doc.search('.titleColumn a').first(5).each do |element|
+    top_url << base_url + element.attribute('href').value
+  end
+
   return top_url
-
-
-  # output
-  # [
-  #   "http://www.imdb.com/title/tt0111161/",
-  #   "http://www.imdb.com/title/tt0068646/",
-  #   "http://www.imdb.com/title/tt0071562/",
-  #   "http://www.imdb.com/title/tt0468569/",
-  #   "http://www.imdb.com/title/tt0050083/"
-  # ]
 end
+
+fetch_movies_url
 
 def scrape_movie(url)
   # scrape individual movie url
+  html_file = open(url, "Accept-Language" => "en").read
+  html_doc = Nokogiri::HTML(html_file)
 
-  # {
-  #   cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
-  #   director: "Christopher Nolan",
-  #   storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-  #   title: "The Dark Knight",
-  #   year: 2008
-  # }
+  movie_hash = {}
+  # need [[:space:]] instead of \s because the space used in the website is not normal space
+  pattern = /(?<title>.*)[[:space:]]\((?<year>\d{4})\)/
+
+  html_doc.search('body').each do |element|
+    title_with_year = element.search('h1').text.strip
+    m_data = title_with_year.match(pattern)
+    
+    movie_hash[:title] = m_data["title"]
+    movie_hash[:year] = m_data["year"].to_i # need to convert year to integer
+
+    movie_hash[:storyline] = element.search('.summary_text').text.strip
+    movie_hash[:director] = element.search('.credit_summary_item').first.search('a').text.strip
+    
+    movie_hash[:cast] = []
+    element.search('.primary_photo + td a').take(3).each do |cast|
+      movie_hash[:cast] << cast.text.strip
+    end
+  end
+
+  return movie_hash
 end
+
+scrape_movie('https://www.imdb.com/title/tt0468569')
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,12 +1,3 @@
-# data for #fetch_movies_url:
-# [
-#   "http://www.imdb.com/title/tt0111161/",
-#   "http://www.imdb.com/title/tt0068646/",
-#   "http://www.imdb.com/title/tt0071562/",
-#   "http://www.imdb.com/title/tt0468569/",
-#   "http://www.imdb.com/title/tt0050083/"
-# ]
-
 require_relative '../scraper'
 
 describe "#fetch_movie_urls" do
@@ -24,14 +15,17 @@ describe "#fetch_movie_urls" do
   end
 end
 
+describe "#scrape_movie" do
+  it 'should scrape movie information by url' do
+    actual = scrape_movie('https://www.imdb.com/title/tt0468569')
+    expected = {
+      cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
+      director: "Christopher Nolan",
+      storyline: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+      year: 2008,
+      title: "The Dark Knight"
+    }
 
-
-
-# data for #scrape_movie:
-# {
-#   cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
-#   director: "Christopher Nolan",
-#   storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-#   title: "The Dark Knight",
-#   year: 2008
-# }
+    expect(actual).to eq(expected)
+  end
+end
